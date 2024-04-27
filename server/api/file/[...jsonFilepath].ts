815f002507fbac610be462d138c3a1ae65f47a92
xOmen.WSL <xliotx@gmail.com>
2024-04-27 15:13:42 +0200

    linting fix.

--- server/api/file/[...jsonFilepath].ts
+++ server/api/file/[...jsonFilepath].ts
@@ -4,6 +4,12 @@ import path from 'node:path'
 import { defineEventHandler } from 'h3'
 
 export default defineEventHandler(async (event) => {
+  // Check if event.context.params is defined
+  if (!event.context.params) {
+    event.node.res.statusCode = 400
+    return { error: 'Missing file path' }
+  }
+
   // Extract the filename from the URL query parameters
   const filePath = event.context.params.jsonFilepath as string
   const fullPath = path.resolve('logs', filePath)
